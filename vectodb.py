716616a184dd06c646b5c69db7299fc91a212a35
AayushSharma-1 <aayush9119@gmail.com>
2024-04-22 23:29:21 +0530

    created_vector_db

--- vectodb.py
+++ vectodb.py
@@ -1,10 +1,28 @@
 from langchain_pinecone import PineconeVectorStore
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from typing import Optional
+import os
+from dotenv import load_dotenv
+from typing import Iterable
+from pinecone import Pinecone, ServerlessSpec
 
+class Upload_db:
+    def upload(DATA, API_KEY, NAME, DIMENSIONS, METRIC, EMBEDDING_MODEL : Optional[str] = 'sentence-transformers/paraphrase-MiniLM-L6-v2'):
+        pc = Pinecone(api_key=API_KEY)
+        pc.create_index(
+            name= NAME,
+            dimension=DIMENSIONS,
+            metric= METRIC,
+            spec =ServerlessSpec(
+                cloud= "aws",
+                region="us-east-1"
+            )
+        )
+        db = PineconeVectorStore.from_documents(DATA, embedding=EMBEDDING_MODEL)
+        return db
 
 class DocSearch:
-    def __init__(self, index_name, api_key, model_name: Optional[str] = None):
+    def __init__(self, index_name, api_key, model_name: Optional[str] = 'sentence-transformers/paraphrase-MiniLM-L6-v2'):
         self.embeddings = HuggingFaceEmbeddings(model_name=model_name)
         self.docsearch = PineconeVectorStore(
             index_name=index_name,
@@ -13,8 +31,9 @@ class DocSearch:
         )
 
 
+
 docsearch = DocSearch(
     model_name="sentence-transformers/paraphrase-MiniLM-L6-v2",
     index_name="quickstart",
-    api_key="fea6d7eb-1b48-4a28-afe5-df253dbe3e1d",
+    api_key=os.getenv("PINECONE_API_KEY"),
 )
