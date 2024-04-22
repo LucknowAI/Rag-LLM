5c6fb1b9579aed08b99ea450d096dbf1788e6ec6
Anshul Yadav <predatorakki0906@gmail.com>
2024-04-23 01:11:53 +0530

    Add new functions for document handling and update API keys

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 import os
+from github import Github, Auth
+from langchain_core.documents import Document
 
 
 def write_to_json(data, filename):
@@ -28,3 +30,35 @@ def fc(user_id):
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
+
+auth = Auth.Token("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")
+g = Github(auth=auth)
+
+
+def unstructure_docs():
+    repo = g.get_repo("LucknowAI/Lucknow-LLM")
+    contents = repo.get_contents("lucknowllm/data/Unstructured_data")
+    text = []
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            text.append(Document(page_content=file_content.decoded_content))
+    return text
+
+
+def create_document_list_from_local(path):
+    document_list = []
+    if os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                with open(os.path.join(root, file), "r") as f:
+                    document_list.append(Document(page_content=f.read()))
+    elif os.path.isfile(path):
+        with open(path, "r") as f:
+            document_list.append(Document(page_content=f.read()))
+    else:
+        print("The path provided is not a valid file or directory")
+    return document_list
