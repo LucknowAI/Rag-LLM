42c89f7fdc610e8d03fb91b548b8f57aec88d867
Akshat Upmanya <akshatupmanya@gmail.com>
2024-04-23 14:58:10 +0530

    Update llms.py

--- pre_final_lallan/llms.py
+++ pre_final_lallan/llms.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+#Here we are using Gemini-1-pro.
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.0-pro",
     convert_system_message_to_human=False,
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=os.getenv("GOOGLE_API_KEY"),#re[place the GOOGLE_API_KEY with your Google API key.
     temperature=0.01,
 )
